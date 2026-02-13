4572f38fa6fb69f80e8a647d0c5efcee38d771f4
plernida <65395927+plernida@users.noreply.github.com>
2026-02-13 14:22:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -287,7 +287,7 @@ def create_dhw_map_mapbox(lon, lat, dhw_data, title):
                         dict(                      
                             sourcetype="geojson",
                             source=coast_geojson,
-                            above="traces",
+                            
                             type="line",
                             minzoom=0,
                             maxzoom=22,
