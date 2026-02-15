f31e165215bd61a5f2d4ead1e4df5dbf0eefae6b
plernida <65395927+plernida@users.noreply.github.com>
2026-02-15 07:11:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -320,7 +320,7 @@ def create_sst_map_mapbox(lon, lat, sst_data, title):
     fig.update_layout(
         title=title,
         mapbox=dict(
-            style="carto-positron",
+            style="stamen-terrain",
             center=dict(lat=7.5, lon=100),
             zoom=4.3
         ),
