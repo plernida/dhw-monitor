7bdc6c060763aad3c14e0c17fa53a4aaccb3e95f
plernida <65395927+plernida@users.noreply.github.com>
2026-02-16 08:59:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -311,10 +311,10 @@ def create_dhw_map_mapbox(lon, lat, dhw_data, title):
         marker_line=dict(width=1, color='black')
     ))
         
-    fig.update_layout(mapbox=dict(style='stamen-watercolor'),  # Or 'carto-positron'
+    fig.update_layout(mapbox=dict(style='stamen-dark'),  # Or 'carto-positron'
                   height=800, margin=dict(r=0, t=40, l=0, b=0))
 
-    fig.update_mapboxes(bounds=dict(bound_east=110,bound_west=90, bound_north=14.5, bound_south=0))
+    fig.update_mapboxes(bounds=dict(east=110,west=90, north=14.5, south=0))
 
     return fig
 
