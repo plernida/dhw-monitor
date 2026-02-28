4191ecadfad5b83fab70ec3b17aca84af664d6ce
plernida <65395927+plernida@users.noreply.github.com>
2026-02-28 11:10:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -519,8 +519,8 @@ if process_button:
                 # SST map
                 fig_sst = create_sst_map_mapbox(lon, lat, sst_current,
                                         f"static/{enddate}_sst.png")
-                fig_sst.update_layout(height=800, margin=dict(l=50,r=20, t=50, b=50))
-                st.plotly_chart(fig_sst, width='stretch')
+                #fig_sst.update_layout(height=800, margin=dict(l=50,r=20, t=50, b=50))
+                #st.plotly_chart(fig_sst, width='stretch')
             with col_right:    
             # Temperature statistics and distribution
                 st.markdown("**SST Statistics**")
