6469fa148d7da676a0cdfca37802f55bdab7730a
plernida <65395927+plernida@users.noreply.github.com>
2026-03-03 11:29:20 +0700

    Update daily_dhw.py

--- daily_dhw.py
+++ daily_dhw.py
@@ -146,7 +146,7 @@ def plot_dhw_week(lon, lat, dhw_week, title, filename):
     # DHW raster
     im = ax.contourf(
         lon2d, lat2d, dhw_week,
-        cmap='RdBu', levels=2,
+        cmap=cmap, levels=2,
         vmin=0, vmax=1,
         transform=ccrs.PlateCarree()
     )
