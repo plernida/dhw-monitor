768b797dc2ef48d51a5d6d7f629155671ca26f89
plernida <65395927+plernida@users.noreply.github.com>
2026-03-04 13:11:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -332,7 +332,7 @@ def plot_dhw_week(lon, lat, dhw_total, title):
     ax.legend(handles=legend_elements,ncol=5,  # Horizontal (5 columns)
            loc='upper center', 
            bbox_to_anchor=(0.5, -0.05),
-          fontsize=10, frameon=True, fancybox=True, shadow=True)
+          fontsize=20, frameon=True, fancybox=True, shadow=True)
     ax.set_title(title, fontsize=20)
     plt.tight_layout()
     #plt.savefig(filename, dpi=150, bbox_inches='tight')
