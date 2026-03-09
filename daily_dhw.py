b4009aaa26f1e792221a404bb7a8f2df312aa472
plernida <65395927+plernida@users.noreply.github.com>
2026-03-10 06:50:57 +0700

    Update daily_dhw.py

--- daily_dhw.py
+++ daily_dhw.py
@@ -288,7 +288,7 @@ def update_bleaching_history(date, value):
 
 # Main daily run
 thtz = pytz.timezone('Asia/Bangkok')
-today = datetime.now(thtz).date() - timedelta(days=2)  # Your app's target
+today = datetime.now(thtz).date() - timedelta(days=3)  # Your app's target
 
 sst_stack, time_list, lat, lon = download_latest_sst(today)
 dhw_weeks, dhw_total, _ = calculate_dhw(sst_stack, MMM)
