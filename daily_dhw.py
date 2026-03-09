313431ec0e1cec6586324e2d9bcf89fb77a38988
plernida <65395927+plernida@users.noreply.github.com>
2026-03-09 08:28:48 +0700

    Update daily_dhw.py

--- daily_dhw.py
+++ daily_dhw.py
@@ -155,7 +155,7 @@ def plot_dhw_week(lon, lat, dhw_week, title, filename):
     ax.xaxis.set_minor_locator(MultipleLocator(1))
     ax.yaxis.set_minor_locator(MultipleLocator(1))
     ax.tick_params(which='both',labeltop=True, labelright=True,labelleft=True,width=0.8,
-                  bottom=True,top=True,right=True,labelsize=10,grid_color='black',grid_linewidth=0.5)
+                  bottom=True,top=True,right=True,labelsize=15,grid_color='black',grid_linewidth=0.5)
     legend_elements = [
         mpatches.Patch(color=colors_rgb[0], label='No stress'),
         mpatches.Patch(color=colors_rgb[1], label='Watch'),
@@ -202,7 +202,7 @@ def plot_dhw_map(lon, lat, dhw_total, filename):
     ax.xaxis.set_minor_locator(MultipleLocator(1))
     ax.yaxis.set_minor_locator(MultipleLocator(1))
     ax.tick_params(which='both',labeltop=True, labelright=True,labelleft=True,width=0.8,
-                  bottom=True,top=True,right=True,labelsize=15,grid_color='black',grid_linewidth=0.5)
+                  bottom=True,top=True,right=True,labelsize=8,grid_color='black',grid_linewidth=0.5)
     # Custom legend patches + labels matching your markdown
     legend_elements = [
         mpatches.Patch(color=colors_rgb[0], label='No stress'),
@@ -216,7 +216,7 @@ def plot_dhw_map(lon, lat, dhw_total, filename):
     ax.legend(handles=legend_elements,ncol=7,  # Horizontal (5 columns)
            loc='upper center', 
            bbox_to_anchor=(0.5, -0.05),
-          fontsize=10, frameon=True, fancybox=True, shadow=True)
+          fontsize=8, frameon=True, fancybox=True, shadow=True)
     
     plt.tight_layout()
     plt.savefig(filename, dpi=150, bbox_inches='tight')
@@ -253,11 +253,11 @@ def create_sst_map_mapbox(lon, lat, sstdata, filename):
     ax.xaxis.set_minor_locator(MultipleLocator(1))
     ax.yaxis.set_minor_locator(MultipleLocator(1))
     ax.tick_params(which='both',labeltop=True, labelright=True,labelleft=True,width=0.8,
-                  bottom=True,top=True,right=True,labelsize=6,grid_color='black',grid_linewidth=0.5)
+                  bottom=True,top=True,right=True,labelsize=8,grid_color='black',grid_linewidth=0.5)
     cbar=fig.colorbar(im,ax=ax,orientation='horizontal', shrink=0.8, pad=0.05)
     cbar.set_ticks(np.arange(24,34.1,1))
-    cbar.set_label('°C',fontsize=6)
-    cbar.ax.tick_params(labelsize=6)
+    cbar.set_label('°C',fontsize=8)
+    cbar.ax.tick_params(labelsize=8)
     #cbar.mappable.set_clim(23, 35)
     plt.tight_layout()
     plt.savefig(filename, dpi=150, bbox_inches='tight')
