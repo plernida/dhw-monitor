2dd6cf12c6b0ea48a16af02a8011bbbfcfc3f651
plernida <65395927+plernida@users.noreply.github.com>
2026-03-10 12:40:22 +0700

    Update daily_dhw.py

--- daily_dhw.py
+++ daily_dhw.py
@@ -308,8 +308,8 @@ try:
         if os.path.exists(nc_path):
             os.remove(nc_path)
     
-    dhw_total.to_netcdf('static/dhw_total.nc')
-    sst_current.to_netcdf('static/sst_current.nc')
+    dhw_total.to_netcdf('/static/dhw_total.nc')
+    sst_current.to_netcdf('/static/sst_current.nc')
     print("NetCDF files saved")
     
     stats = {
