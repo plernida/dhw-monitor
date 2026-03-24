a76797875899bcec58efb272afe3f47ce9c352d6
plernida <65395927+plernida@users.noreply.github.com>
2026-03-24 09:21:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -411,7 +411,7 @@ def create_dhw_map(lon, lat, dhw_total, title):
         #contours_coloring="fill",
         contours=dict(
             start= 0,
-            end=6,
+            end=7,
             size=1,
             showlines=False,
             #labelfont=dict(size=12,color="black"),
