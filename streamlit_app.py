f2763e7e2a1dc5b55468e3049820265ab3dfed9a
plernida <65395927+plernida@users.noreply.github.com>
2026-03-30 11:08:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -833,25 +833,25 @@ with st.spinner('Processing DHW analysis...'):
                 
                     idx = np.argmin(distances)
                     station_name = thai_stations['name'][idx]
-                st.subheader(f"📈 SST History — {station_name}")
+                
 
-                sst_series = station_histories[station_name]["sst"]
-            
-                fig_ts = go.Figure()
-            
-                fig_ts.add_trace(go.Scatter(
-                    y=sst_series,
-                    mode='lines+markers',
-                    name=station_name
-                ))
-            
-                fig_ts.update_layout(
-                    height=300,
-                    xaxis_title="Days (last 30)",
-                    yaxis_title="SST (°C)",
-                    margin=dict(l=20, r=20, t=40, b=20)
-                )
-            
+                    sst_series = station_histories[station_name]["sst"]
+                
+                    fig_ts = go.Figure()
+                
+                    fig_ts.add_trace(go.Scatter(
+                        y=sst_series,
+                        mode='lines+markers',
+                        name=station_name
+                    ))
+                
+                    fig_ts.update_layout(
+                        height=300,
+                        xaxis_title="Days (last 30)",
+                        yaxis_title="SST (°C)",
+                        margin=dict(l=20, r=20, t=40, b=20)
+                    )
+                
                 st.plotly_chart(fig_ts, width='stretch')
             #st.plotly_chart(fig_dhw, width='stretch')
                         
