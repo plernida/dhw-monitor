218ca6119c4c0f308d5627b9e16f4a23944e2657
plernida <65395927+plernida@users.noreply.github.com>
2026-09-08 12:48:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -957,7 +957,7 @@ with st.spinner('Processing DHW analysis...'):
                             fig = create_dhw_weeks(lon, lat, dhw_weeks[week_idx],
                                                date_labels[week_idx])
                             fig.update_layout(height=350)
-                            st.plotly_chart(fig, weight='stretch')
+                            st.plotly_chart(fig, width='stretch')
 
                         
     
